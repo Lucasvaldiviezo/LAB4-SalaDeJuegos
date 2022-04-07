f65de79bc4519c5535637b73ecda60e3a14d727e
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2022-04-07 20:50:31 -0300

    routing agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { JuegoComponent } from './page/juego/juego.component';
+import { AhorcadoComponent } from './page/ahorcado/ahorcado.component';
+import { QuienSoyComponent } from './quien-soy/quien-soy.component';
 
-
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'sobreMi',component:QuienSoyComponent},
+  {path:'juegos',component:JuegoComponent, children: [ 
+      {path: 'ahorcado',component: AhorcadoComponent },
+    ] 
+  },
+  {path:'**', component:NotFoundComponent}
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { QuienSoyComponent } from './quien-soy/quien-soy.component';
 import { HomeComponent } from './home/home.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
+import { AhorcadoComponent } from './page/ahorcado/ahorcado.component';
+import { JuegoComponent } from './page/juego/juego.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     QuienSoyComponent,
-    HomeComponent
+    HomeComponent,
+    NavmenuComponent,
+    NotFoundComponent,
+    AhorcadoComponent,
+    JuegoComponent
   ],
   imports: [
     BrowserModule,
