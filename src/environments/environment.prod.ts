477d95dce18858f17fccb42a2c34dd340ee0357f
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2022-04-18 20:05:27 -0300

    avanzando sprint 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBYYJNfesjB-gp0jEYkmifbpLIJxmDEprU",
+    authDomain: "saladejuegos-tp.firebaseapp.com",
+    projectId: "saladejuegos-tp",
+    storageBucket: "saladejuegos-tp.appspot.com",
+    messagingSenderId: "853745751884",
+    appId: "1:853745751884:web:fa9f39f2fc81b80ba121e9",
+    measurementId: "G-FN6QBP4KSB"
+  }
 };
