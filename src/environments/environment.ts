0478e3253773a86fcdf73b3f090d835080fa8362
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2022-04-19 20:33:19 -0300

    probando

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig : {
     apiKey: "AIzaSyBYYJNfesjB-gp0jEYkmifbpLIJxmDEprU",
     authDomain: "saladejuegos-tp.firebaseapp.com",
